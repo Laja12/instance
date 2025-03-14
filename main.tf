89201cd0e0e9bddb3c8145730bd33d7b70f8cb84
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-03-14 16:01:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,13 @@ resource "aws_instance" "example" {
   ami           = "ami-0013d898808600c4a"
   instance_type = "t2.micro"               # Replace with your desired instance type
 
-  user_data = <<-EOF
+  user_data =  <<-EOF
               #!/bin/bash
-              echo "Welcome to EC2 Instance ${count.index + 1}" > /var/www/html/index.html
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<html><body><h1>Welcome to My EC2 Instance ${count.index + 1}!</h1><p>Your custom message goes here.</p></body></html>" > /var/www/html/index.html
               EOF
 
   tags = {
