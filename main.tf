527f18fc5ae3735ec299bb17940b4efd3b138883
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-03-14 14:55:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ resource "aws_instance" "example" {
   ami           = "ami-0013d898808600c4a"
   instance_type = "t2.micro"               # Replace with your desired instance type
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Welcome to EC2 Instance ${count.index + 1}" > /var/www/html/index.html
+              EOF
+
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
   }
