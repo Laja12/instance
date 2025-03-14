b26a76fb8ea5ad2d669a229948567ce69b058045
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-03-14 14:35:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   count         = 3
-
+  ami           = "ami-0041fa54d2b785bb0"
   instance_type = "t2.micro"               # Replace with your desired instance type
 
   tags = {
