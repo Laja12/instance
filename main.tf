dc963a037d0b630e7901e505ac94a7431ec08c10
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-03 17:40:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,46 @@
 provider "aws" {
-  region = "ap-southeast-2"  # Replace with your desired AWS region
+  region = "ap-southeast-2"
 }
 
-resource "aws_instance" "example" {
-  count         = 3
-  ami           = "ami-0013d898808600c4a"
-  instance_type = "t2.micro"               # Replace with your desired instance type
+resource "aws_iam_user" "awscliuser" {
+  name = "awscliuser"
+}
 
-  user_data =  <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "<html><body><h1>Welcome to My EC2 Instance ${count.index + 1}!</h1><p>Your custom message goes here.</p></body></html>" > /var/www/html/index.html
-              EOF
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.awscliuser.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
 
-  tags = {
-    Name = "ExampleInstance-${count.index + 1}"
-  }
+resource "aws_iam_access_key" "awscliuser_access_key" {
+  user = aws_iam_user.awscliuser.name
+}
+
+resource "aws_iam_role" "s3_role" {
+  name = "ec2-s3-access"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_s3_access_policy_attachment" {
+  role       = aws_iam_role.s3_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+output "awscliuser_access_key_id" {
+  value = aws_iam_access_key.awscliuser_access_key.id
+}
+
+output "awscliuser_secret_access_key" {
+  value = aws_iam_access_key.awscliuser_access_key.secret
+  sensitive = true
 }
